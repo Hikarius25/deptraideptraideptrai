f16139aa2ab11a9ec51a98a958d23e0fb9ed24a9
Hikarius25 <164138661+Hikarius25@users.noreply.github.com>
2024-07-11 08:32:43 +0700

    commit3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + './deptrai.html');
+    res.sendFile(__dirname + '/public/deptrai.html');
 });
 
 app.listen(port, () => {
